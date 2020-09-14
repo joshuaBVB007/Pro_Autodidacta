3aba97e32b4d5781605e77d6309f86d27d94b022
joshuaBVB007 <56931544+joshuaBVB007@users.noreply.github.com>
2020-09-14 17:37:09 +0200

    version topo

--- PRO/src/swing/Dibujos.java
+++ PRO/src/swing/Dibujos.java
@@ -1,7 +1,9 @@
 package swing;
-
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.FlowLayout;
 import java.awt.Graphics;
+import java.awt.GridLayout;
 import java.awt.Image;
 import java.io.File;
 import java.io.IOException;
@@ -21,21 +23,51 @@ public class Dibujos {
 }
 class frame extends JFrame{
 	public frame(){
-		setBounds(20, 20, 500, 500);
+		setBounds(20, 20, 300, 200);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
-		add(new Lamina());
+		add(new Lamina(),BorderLayout.NORTH);
+		add(new Lamina2(),BorderLayout.CENTER);
+		//pack();
 	}
 }
 class Lamina extends JPanel{
-	private JButton B1=new JButton("hola");
-	private JButton B2=new JButton("hola");
-	private JButton B3=new JButton("hola");
+	private JButton B1=new JButton("0");
 	public Lamina(){
-		setBackground(Color.black);
-		add(B1);
-		add(B2);
+		setLayout(new BorderLayout());
+		add(B1,BorderLayout.NORTH);
+		B1.setEnabled(false);
+		
+	}
+}
+class Lamina2 extends JPanel{
+	private JButton B9=new JButton("9");
+	private JButton B8=new JButton("8");
+	private JButton B7=new JButton("7");
+	private JButton B6=new JButton("6");
+	private JButton B5=new JButton("5");
+	private JButton B4=new JButton("4");
+	private JButton B3=new JButton("3");
+	private JButton B2=new JButton("2");
+	private JButton B1=new JButton("1");
+	private JButton BPunto=new JButton(".");
+	private JButton BIgual=new JButton("=");
+	private JButton B0=new JButton("0");
+	public Lamina2(){
+		GridLayout disposicion2=new GridLayout(4,4);
+		setLayout(disposicion2);
+		add(B9);
+		add(B8);
+		add(B7);
+		add(B6);
+		add(B5);
+		add(B4);
 		add(B3);
+		add(B2);
+		add(B1);
+		add(BPunto);
+		add(BIgual);
+		add(B0);
 	}
 }
 

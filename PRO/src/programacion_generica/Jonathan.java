b19e9dcc7c8bf89d3f7449d6630cb1ef2c775707
joshuaBVB007 <56931544+joshuaBVB007@users.noreply.github.com>
2020-10-04 20:37:26 +0200

    version generica

---
+++ PRO/src/programacion_generica/Jonathan.java
@@ -0,0 +1,18 @@
+package programacion_generica;
+
+public class Jonathan <T>{
+	private T general;
+	
+	public Jonathan(){
+		general=null;
+	}
+	
+	public T getGeneral() {
+		return general;
+	}
+
+	public void setGeneral(T general) {
+		this.general = general;
+	}
+
+}
---
+++ PRO/src/programacion_generica/UsoJonathan.java
@@ -0,0 +1,14 @@
+package programacion_generica;
+
+public class UsoJonathan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Jonathan <String> lista=new Jonathan <String>();
+		lista.setGeneral("Jesus");
+		
+		System.out.println(lista.getGeneral());
+
+	}
+}
+
